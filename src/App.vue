7aac3725838d73db778260b77af1d0f4b8860a76
Nastya Borisova <borisova.n@stik.pro>
2023-11-27 01:37:43 +0300

    VT-1 | add Header and Navigation components

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <script setup>
-import { CheckIcon } from '@heroicons/vue/24/solid'
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-
-const navList = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon,
-}
+import Header from './components/header/Header.vue'
+import Navigation from './components/navigation/Navigation.vue'
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a>Logo</a>
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex items-center gap-1">
-        Day complete!
-        <CheckIcon class="h-7 text-green-500"/>
-      </div>
-      <div v-else class="flex items-center gap-1">
-        Progress:
-        <span class="font-mono">20%</span>
-        <span class="h-3 w-3 rounded full bg-red-500"></span>
-      </div>
-    </a>
-  </header>
+  <Header />
 
   <main class="flex flex-grow flex-col">
     On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
   </main>
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navList" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6"/>
-          {{ page }}
-        </a>
-      </li>
-    </ul>
-  </nav>
+
+  <Navigation />
 </template>
 
 <style scoped>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { CheckIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a>Logo</a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckIcon class="h-7 text-green-500"/>
+      </div>
+      <div v-else class="flex items-center gap-1">
+        Progress:
+        <span class="font-mono">20%</span>
+        <span class="h-3 w-3 rounded full bg-red-500"></span>
+      </div>
+    </a>
+  </header>
+</template>
