c517ce4928b03130a9cc90ac1d9e873d41191a00
Nastya Borisova <borisova.n@stik.pro>
2023-12-17 15:57:03 +0300

    VT-1 | edit navigation component

--- src/components/navigation/MainNavigation.vue
+++ src/components/navigation/MainNavigation.vue
@@ -1,22 +1,28 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavigationItem from './NavigationItem.vue'
 
 const navList = {
   timeline: ClockIcon,
   activities: ListBulletIcon,
   progress: ChartBarIcon,
 }
+
+const currentPage = 'timeline'
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navList" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6"/>
-          {{ page }}
-        </a>
-      </li>
+      <NavigationItem
+        v-for="(icon, page) in navList"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-grey-200': currentPage === 'timeline' }"
+      >
+        <component :is="icon" class="h-6"/>
+        {{ page }}
+      </NavigationItem>
     </ul>
   </nav>
 </template>
